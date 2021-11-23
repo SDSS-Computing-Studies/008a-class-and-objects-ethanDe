5738c28848dd89701c934628efe148bee8be4597
ethanDe <ethand971@deltalearns.ca>
2021-11-23 14:47:34 -0800

    Done

--- task1.py
+++ task1.py
@@ -13,7 +13,7 @@ It should have the following methods:
 average()       - determines the mathematical average of all course grades
 getHonorRoll()  - determines the average of the top 5 courses if there are at least 5 courses.
                   returns True if the average is 86 or higher (on the honor roll)
-                  returns False if not on the honor roll
+                  returns False if not on the honor roll 
 showCourses()   - prints a list of all courses
 showGrade(int)  - takes 1 parameter, the index of the list
                 - displays the course name and grade
@@ -33,23 +33,28 @@ class student:
 
 
 
+    def __init__(self, name, studentNumber, grade): # You will need to create your own input parameters for all methods
+        self.name = name
+        self.studentNumber = studentNumber
+        self.grade = grade
 
-    def student(self, name, studentNumber, grade): # You will need to create your own input parameters for all methods
-        self.name = str(name)
-        self.studentNumber = str(studentNumber)
-        self.grade = int(grade)
 
-        pass
-
-    def course(self, courses, grades):
+    def getCourses(self, courses):
         self.courses = courses
-        self.grades = grades
-        
-        pass
+
+    def getGrades(self, grades1):
+        self.grades = grades1
 
     def average(self):
-        
-        pass
+        classes = len(self.grades)
+        average = sum(self.grades) / classes
+        return average
+
+    def getHonorRoll(self):
+        self.grades.sort(reverse = True)
+        average = self.grades[0] + self.grades[1] + self.grades[2] + self.grades[3] + self.grades[4] / 5
+        if average >= 86:
+            return True
 
 def main():
     # This contains test data that will be used by the autograder.
@@ -62,8 +67,5 @@ def main():
     st2 = student("Joe Lunchbox","12346", 11)
     st2.getCourses( ["English","Math","Physics","Computers","Geography","Chemistry","French"] )
     st2.getGrades( [71, 98, 93, 95, 68, 81, 71])
-
-
-
 
 main()
